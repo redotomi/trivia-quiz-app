9a557d758ea2c9c144ab3fb57cfdb443b6eced6c
redotomi <tomy.redondo001@gmail.com>
2022-08-09 18:39:53 -0300

    Crea seccion de preguntas

---
+++ src/components/quiz-section/QuizSection.js
@@ -0,0 +1,14 @@
+import Question from "./Question";
+import '../../style.css'
+
+export default function QuizSection() {
+  return (
+    <div>
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+    </div>
+  );
+}
